4aa127c7c7ec18ac0fbf8826b415e3fbd799edc8
Alexios Karadimos <karadalex@gmail.com>
2019-12-07 22:03:25 +0200

    Create new fresh ASP.Net Core 3.0 MVC webapp

---
+++ webapp/Clintelli/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Clintelli.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
